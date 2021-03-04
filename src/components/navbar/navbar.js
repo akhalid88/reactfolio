41952936325d092e08a00746ac9e088bdfa2b275
Muhammad K <akhalid.code@gmail.com>
2021-03-03 21:23:14 -0800

    add reactbooks project

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { AppBar, Tabs, Tab, Button } from '@material-ui/core';
+import React from 'react';
+import { AppBar, Tabs, Tab} from '@material-ui/core';
 
 import Home from "../../pages/home";
 import Portfolio from "../../pages/portfolio";
